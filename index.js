dc2769d2a8aa4076c603aedaca01340267c56c33
Jack <29169102+Jack5079@users.noreply.github.com>
2020-07-06 22:30:14 -0400

    fix icon

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, "index.html"));
   mainWindow.menuBarVisible = false
-  mainWindow.setIcon('./icon.ico')
 };
 
 // This method will be called when Electron has finished
